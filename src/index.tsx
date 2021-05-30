2f8efba45646610b84f3ab6c2cba2d18405c8b39
zchung1 <zchung1@its.jnj.com>
2021-05-30 15:36:24 +0800

    JSX JavaScript Demo

--- src/index.tsx
+++ src/index.tsx
@@ -10,15 +10,18 @@ function BookList() {
   )
 }
 
+const author = 'Amelia Hepworth'
+
 const Book = () => {
+  const title = 'I Love You to the Moon and Back'
   return (
     <article className="book">
       <img
         src="https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg"
         alt=""
       />
-      <h1>I Love You to the Moon and Back</h1>
-      <h4>Amelia Hepworth</h4>
+      <h1>{title}</h1>
+      <h4>{author.toUpperCase()}</h4>
     </article>
   )
 }
